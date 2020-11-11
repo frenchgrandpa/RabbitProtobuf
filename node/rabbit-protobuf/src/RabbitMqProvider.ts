db09057b26f72d9156cc2e2aac5059dacecab2dd
French Grandpa <pepijn.pinckers@hotmail.nl>
2020-11-11 16:43:03 +0100

    Added rabbitmq error handler

--- node/rabbit-protobuf/src/RabbitMqProvider.ts
+++ node/rabbit-protobuf/src/RabbitMqProvider.ts
@@ -1,4 +1,12 @@
 import * as rabbitMq from "amqplib";
+import set = Reflect.set;
+
+
+interface Settings {
+    host?: string;
+    username?: string;
+    password?: string;
+}
 
 
 class RabbitMqProvider {
@@ -7,10 +15,11 @@ class RabbitMqProvider {
 
     public channel!: rabbitMq.Channel;
     private connection!: rabbitMq.Connection;
+    private handlingError = false;
 
 
 
-    public async initAsync(settings?: { host?: string; username?: string; password?: string }): Promise<void> {
+    public async initAsync(settings?: Settings): Promise<void> {
         settings = settings || {};
         const host = settings.host || "localhost";
         const username = settings.username || "guest";
@@ -23,6 +32,36 @@ class RabbitMqProvider {
             durable: true
         });
 
+        this.connection.on("error", (e) => this.onError(e, settings));
+        this.channel.on("error", (e) => this.onError(e, settings));
+
+    }
+
+
+
+    private onError(err: Error, settings: Settings | undefined): void {
+        console.log("An error occured to the RabbitMQ connection:");
+        console.log(err)
+
+        if (this.handlingError) {
+            console.log("Cant handle error because another error is currently being handled");
+            return;
+        }
+
+        setTimeout(async () => {
+            try {
+                await this.disposeAsync();
+            } catch (e) {
+                console.log("An error occurred while disposing the RabbitMQ connection while reconnecting due to an error:");
+                console.log(e);
+            } finally {
+                setTimeout(async () => {
+                    console.log("Reconnecting to RabbitMQ...");
+                    await this.initAsync(settings);
+                    this.handlingError = false;
+                }, 3000);
+            }
+        }, 3000);
     }
 
 
@@ -30,6 +69,7 @@ class RabbitMqProvider {
     public async disposeAsync(): Promise<void> {
         await this.channel.close();
         await this.connection.close();
+        console.log("Disposed RabbitMQ connection")
     }
 
 }
